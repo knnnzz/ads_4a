c965e1cb4fd7e5fe4bd68034d145157b9f510d64
Kauan <160064587+knnnzz@users.noreply.github.com>
2024-05-16 20:47:54 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,6 +19,10 @@ app.get('/cadastro', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/validar', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'validate.html'));
+});
+
 app.get('/', (req, res) => {
   res.send('Servidor ligado com sucesso!');
 });
